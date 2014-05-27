a84e74cc12aa4baeaffd352d5a1f445863b3ac39
nmmarquez <nmmarquez@gmail.com>
2014-05-27 14:34:33 -0400

    corrected option for Davids score algorithm

--- R/davidScoreLA.R
+++ R/davidScoreLA.R
@@ -2,10 +2,10 @@
 #' 
 #' Calculates the David's Score of players in an "interData" object.
 #' @param intData object of class "interData" to calculate scores.
+#' @param corrected specify wether to use David's adjustment for chance. 
 #' @details Using the methods outlined in Gamel et al. 2003 a David's score is 
-#' calculated using interactions from intData. Adjusting the players parameter
-#' will modify the algorithm so that only interactions that involve the players
-#' specified will be used.
+#' calculated using interactions from intData. Adjusting the corrected parameter
+#' will modify the algorithm to use David's adjustment for chance. 
 #' @return A 2 column data frame specifying the players used in the algorithm 
 #' sorted by their corresponding david's score.
 #' @examples
@@ -16,16 +16,23 @@
 #'                             d = Sys.time () + runif (100, 40, 160))
 #' # convert to interData object
 #' id1 <- intTableConv (interactions)
-#' # calculate using all players
+#' # calculate David's Score
 #' davidScore (id1)
+#' # with David's adjustment for chance
+#' davidScore (id1, corrected = TRUE)
 #' @references Gammel et al. (2003) David's Score. Animal Behaviour
 #' @export
 
-davidScore <- function (intData){
+davidScore <- function (intData, corrected = FALSE){
     idError (intData); plyrs <- intData$players
-    Pmat <- sapply (plyrs, function (x) 
-        sapply (plyrs, function (y) Pij (y, x, intData)))
-    
+    if (corrected){
+        Pmat <- sapply (plyrs, function (x) 
+            sapply (plyrs, function (y) Dij (y, x, intData)))
+    }
+    else{
+        Pmat <- sapply (plyrs, function (x) 
+            sapply (plyrs, function (y) Pij (y, x, intData)))
+    }
     w <- rowSums (Pmat); l <- colSums (Pmat)
     w2 <- Pmat %*% w; l2 <- t (t(l) %*% Pmat)
     
